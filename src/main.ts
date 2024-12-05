acaff560a44c78545e3d237c981a209adb51b0a1
LouisRvl <louis.reville@gmail.com>
2024-12-05 13:56:33 +0000

    first commit

---
+++ src/main.ts
@@ -0,0 +1,56 @@
+import express from "express";
+import { Request, Response } from "express";
+import { Product } from "./entity/product.entity.js";
+import { db } from "./db.js";
+
+db.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!");
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization:", err);
+    });
+
+const app = express();
+app.use(express.json());
+
+app.get("/products", async function (req: Request, res: Response) {
+    const products = await db.getRepository(Product).find();
+    res.json(products);
+});
+
+app.get(
+    "/products/:id",
+    async function (req: Request<{ id: number }>, res: Response) {
+        const results = await db.getRepository(Product).findOneBy({
+            id: req.params.id,
+        });
+        res.send(results);
+    }
+);
+
+app.post("/products", async function (req: Request, res: Response) {
+    const product = db.getRepository(Product).create(req.body);
+    const results = await db.getRepository(Product).save(product);
+    res.send(results);
+});
+
+app.put("/products/:id", async function (req: Request, res: Response) {
+    if (isNaN(Number(req.params.id))) {
+        res.status(400).send({ error: "Invalid product ID" });
+    }
+    const id = parseInt(req.params.id);
+    const product = await db.getRepository(Product).findOneBy({
+        id,
+    });
+    db.getRepository(Product).merge(product, req.body);
+    const results = await db.getRepository(Product).save(product);
+    res.send(results);
+});
+
+app.delete("/products/:id", async function (req: Request, res: Response) {
+    const results = await db.getRepository(Product).delete(req.params.id);
+    res.send(results);
+});
+
+app.listen(3000);
