e6bfb00ba33565b6bc46800620ea2adc81207c97
LouisRvl <louis.reville@gmail.com>
2024-12-05 18:23:10 +0000

    Refactor API endpoints to manage users instead of products

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { Request, Response } from "express";
-import { Product } from "./entity/product.entity.js";
+import { User } from "./entity/user.entity.js";
 import { db } from "./db.js";
 import * as dotenv from "dotenv";
 dotenv.config();
@@ -18,42 +18,42 @@ db.initialize()
 const app = express();
 app.use(express.json());
 
-app.get("/products", async function (req: Request, res: Response) {
-    const products = await db.getRepository(Product).find();
-    res.json(products);
+app.get("/users", async function (req: Request, res: Response) {
+    const users = await db.getRepository(User).find();
+    res.json(users);
 });
 
 app.get(
-    "/products/:id",
+    "/users/:id",
     async function (req: Request<{ id: number }>, res: Response) {
-        const results = await db.getRepository(Product).findOneBy({
+        const results = await db.getRepository(User).findOneBy({
             id: req.params.id,
         });
         res.send(results);
     }
 );
 
-app.post("/products", async function (req: Request, res: Response) {
-    const product = db.getRepository(Product).create(req.body);
-    const results = await db.getRepository(Product).save(product);
+app.post("/users", async function (req: Request, res: Response) {
+    const user = db.getRepository(User).create(req.body);
+    const results = await db.getRepository(User).save(user);
     res.send(results);
 });
 
-app.put("/products/:id", async function (req: Request, res: Response) {
+app.put("/users/:id", async function (req: Request, res: Response) {
     if (isNaN(Number(req.params.id))) {
-        res.status(400).send({ error: "Invalid product ID" });
+        res.status(400).send({ error: "Invalid user ID" });
     }
     const id = parseInt(req.params.id);
-    const product = await db.getRepository(Product).findOneBy({
+    const user = await db.getRepository(User).findOneBy({
         id,
     });
-    db.getRepository(Product).merge(product, req.body);
-    const results = await db.getRepository(Product).save(product);
+    db.getRepository(User).merge(user, req.body);
+    const results = await db.getRepository(User).save(user);
     res.send(results);
 });
 
-app.delete("/products/:id", async function (req: Request, res: Response) {
-    const results = await db.getRepository(Product).delete(req.params.id);
+app.delete("/users/:id", async function (req: Request, res: Response) {
+    const results = await db.getRepository(User).delete(req.params.id);
     res.send(results);
 });
 
