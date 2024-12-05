4c96a62081dcdedb9f3897ae1336cacbe1924ff8
LouisRvl <louis.reville@gmail.com>
2024-12-05 15:49:17 +0000

    Update database configuration and initialization in db.ts for MySQL

--- src/db.ts
+++ src/db.ts
@@ -1,17 +1,29 @@
 import { DataSource } from "typeorm";
-import * as dotenv from "dotenv";
-dotenv.config();
 
-const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+const { DB_HOST, MYSQL_TCP_PORT, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } =
+    process.env;
 
 export const db = new DataSource({
     type: "mysql",
     host: DB_HOST,
-    port: parseInt(DB_PORT),
-    username: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    entities: ["src/entity/*.js"],
+    port: parseInt(MYSQL_TCP_PORT),
+    username: MYSQL_USER,
+    password: MYSQL_PASSWORD,
+    database: MYSQL_DATABASE,
+    entities: ["dist/entity/*.js"],
     logging: true,
     synchronize: true,
 });
+
+db.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!");
+        console.log("total of ", db.entityMetadatas.length, "entities");
+        db.entityMetadatas.forEach((entity) => {
+            console.log("Entity:", entity.name);
+        });
+        db.runMigrations();
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization:", err);
+    });
